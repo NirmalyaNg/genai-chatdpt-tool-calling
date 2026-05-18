85622efd8e3e710d399d24c204e858b81683fd6e
Nirmalya <nirmalyaganguly1999@gmail.com>
2026-05-18 13:59:31 +0530

    Server frontend from express server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { generate, getThreadMessages } from './chatbot.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8000;
+const staticDir = path.join(__dirname, 'frontend', 'dist');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(staticDir));
 
 app.get('/chat/:threadId', async (req, res) => {
   const { threadId } = req.params ?? {};
@@ -52,6 +58,10 @@ app.post('/chat', async (req, res) => {
       error: 'Failed to generate response',
     });
   }
+});
+
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(staticDir, 'index.html'));
 });
 
 app.listen(PORT, () => {
